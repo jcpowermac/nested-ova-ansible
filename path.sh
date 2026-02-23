406c008ec6ed0f6f875dc4133bbd588ba0215ef7
Joseph Callen <jcallen@redhat.com>
2026-02-23 08:42:44 -0500

    working

---
+++ path.sh
@@ -0,0 +1,2 @@
+
+export PATH=/home/jcallen/Development/nested-ova-ansible/.venv/bin:/home/jcallen/.opencode/bin:/home/jcallen/.npm-global/bin:/home/jcallen/.opencode/bin:/home/jcallen/.npm-global/bin:/home/jcallen/.local/bin:/usr/lib64/ccache:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/jcallen/.local/share/JetBrains/Toolbox/scripts
